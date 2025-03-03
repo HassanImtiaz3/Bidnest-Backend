202742b065e5d0ddea83031e0d152d65ba8c9760
Hassan <hassanimtiazip@gmail.com>
2025-03-04 01:54:02 +0500

    random

--- mailer/mail.js
+++ mailer/mail.js
@@ -1,22 +1,27 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 class EmailService {
   static async sendEmail(name, email, phone, message) {
     try {
       const transporter = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
-          user: "f03da1ac9d4220",
-          pass: "25d672e4e14631"
-        }
+          user: "bbidnest2@gmail.com",
+          pass: "Riphah@0306",
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
       });
 
       const mailOptions = {
-        from: `"BidNest Support" <support@bidnest.com>`,
-        to: "hassan.imtiaz@visnext.net",
+        from: email,
+        to: "hassanimtiazip@gmail.com",
         subject: "New Contact Form Submission",
         html: `
           <h3>New Contact Form Submission</h3>
@@ -28,7 +33,11 @@ class EmailService {
       };
 
       const emailResult = await transporter.sendMail(mailOptions);
-      return { success: true, message: "Email sent successfully!", data: emailResult };
+      return {
+        success: true,
+        message: "Email sent successfully!",
+        data: emailResult,
+      };
     } catch (error) {
       console.error("Email sending error:", error);
       return { success: false, message: "Failed to send email." };
